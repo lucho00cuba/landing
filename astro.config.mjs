b44702cf9018c8f062b1574342951e8477545a28
lucho00cuba <luisoctavio.mota.verdasco@gmail.com>
2026-06-10 00:15:02 +0200

    chore(config): switch from Node adapter to static output and remove related dependencies
    - update Astro configuration to output static files instead of server
    - remove @astrojs/node dependency from package.json and pnpm-lock.yaml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 // @ts-check
-import node from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://justme.ovh",
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  output: "static",
   integrations: [sitemap()],
 });
